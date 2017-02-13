961adddfe782fa29fc1a5dcc3a53992e44b53cfb
Dennis von der Bey <dvonderbey@communicode.de>
2017-02-13 11:49:02 +0100

    Added loaders to allow successful webpack serving

--- webpack.config.js
+++ webpack.config.js
@@ -3,29 +3,39 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-  context: path.join(__dirname, "src"),
-  devtool: debug ? "inline-sourcemap" : null,
-  entry: "./js/client.js",
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-0'],
-          plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
+    context: path.join(__dirname, "src"),
+    devtool: debug ? "inline-sourcemap" : null,
+    entry: "./js/client.js",
+    module: {
+        loaders: [
+        {
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel-loader',
+            query:
+            {
+                presets: ['react', 'es2015', 'stage-0'],
+                plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
+            }
+        },
+        {
+            test: /\.css$/,
+            loaders: ['style-loader', 'css-loader']
+        },
+        {
+            test: /\.(eot|png|svg|[ot]tf|woff2?)(\?v=\d+\.\d+\.\d+)?$/,
+            loader: 'url-loader',
+            query: {limit: 10000}
         }
-      }
     ]
   },
-  output: {
-    path: __dirname + "/src/",
-    filename: "client.min.js"
+    output: {
+        path: __dirname + "/src/",
+        filename: "client.min.js"
   },
-  plugins: debug ? [] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-  ],
+    plugins: debug ? [] : [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+    ]
 };
