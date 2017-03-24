4869004bf911bd0ea3f3134464059ce468b68525
Dennis von der Bey <dvonderbey@communicode.de>
2017-03-24 10:54:14 +0100

    Fixed indentation

--- src/js/components/Index.js
+++ src/js/components/Index.js
@@ -6,31 +6,31 @@ import "../../css/components/Index.css";
 import { Card, Grid, Segment, Divider } from 'semantic-ui-react'
 
 export default class Index extends React.Component {
-    render() {
-        return (
-          <div>
-              <Header/>
-              <NavBar/>
-              <div class="content">
-                  <Segment raised>
-                      <h1>Company internal:</h1>
-                      <Divider />
-                      <div class="asd">
-                          <Grid stackable>
-                            <Grid.Column>
-                                <KeyCard></KeyCard>
-                            </Grid.Column>
-                      </Grid>
-                      </div>
-                      <Divider />
-                      <Grid stackable>
-                          <Grid.Column>
-                              <KeyCard></KeyCard>
-                          </Grid.Column>
-                      </Grid>
-                  </Segment>
-              </div>
-          </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Header/>
+        <NavBar/>
+        <div class="content">
+          <Segment raised>
+            <h1>Company internal:</h1>
+            <Divider />
+            <div class="asd">
+              <Grid stackable>
+                <Grid.Column>
+                  <KeyCard></KeyCard>
+                </Grid.Column>
+              </Grid>
+            </div>
+            <Divider />
+            <Grid stackable>
+              <Grid.Column>
+                <KeyCard></KeyCard>
+              </Grid.Column>
+            </Grid>
+          </Segment>
+        </div>
+      </div>
+    )
+  }
 }
