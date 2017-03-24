3e7436ea6324f5e2ac26cb677047f8ba332ccf19
Dennis von der Bey <dvonderbey@communicode.de>
2017-03-24 10:58:27 +0100

    Fixed indentation

--- src/js/components/NavBar.js
+++ src/js/components/NavBar.js
@@ -3,13 +3,14 @@ import { Image, Icon, Menu } from 'semantic-ui-react'
 import "../../css/components/NavBar.css";
 
 export default class NavBar extends Component {
-    state = { activeItem: 'home' }
-    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-    render() {
-      const { activeItem } = this.state
-      return (
-        <div class="navbar">
-        </div>
-      )
-    }
+  state = { activeItem: 'home' }
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  render() {
+    const { activeItem } = this.state
+    return (
+      <div class="navbar">
+        ASD
+      </div>
+    )
+  }
 }
