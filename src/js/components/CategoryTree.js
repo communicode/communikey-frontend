99dcee8e4ce397d89ceed9c2d22f569c29de24b8
Dennis von der Bey <dvonderbey@communicode.de>
2017-03-29 15:25:19 +0200

    Deleted unwanted testing view of keycards

--- src/js/components/CategoryTree.js
+++ src/js/components/CategoryTree.js
@@ -30,24 +30,9 @@ class CategoryTree extends Component {
       )
     })
     return (
-      <div>
       <Card>
         { categoryList }
       </Card>
-      <Card>
-        <Card.Content header='Card 1' />
-        <Card.Meta>
-          <span className='date'>
-            Created 23rd January 2016
-          </span>
-        </Card.Meta>
-        <Card.Content description="Lorem ipsum dolor sit amet" />
-        <Card.Content extra>
-          <Image src='../../img/communikey-logo.svg' avatar />
-          <span>Username</span>
-        </Card.Content>
-      </Card>
-    </div>
       )
     }
   }
