b6b763e3b8803908bee26659e0befd01e1bca499
Marius Skyschally <mskyschally@communicode.de>
2017-04-06 11:15:45 +0200

    Added Administration view for user management with a list of all users and a detail-user-view for any user. (Only layout)

--- src/js/components/Admin.js
+++ src/js/components/Admin.js
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { Component } from 'react'
+import { Image, List, Container, Header } from 'semantic-ui-react'
+import UserDetails from "./UserDetails";
+import "../../css/components/Admin.css";
 
 export default class Admin extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { show : false};
+
+    this.showUserDetails = this.showUserDetails.bind(this)
+  }
+
+  showUserDetails = () => {
+    console.log("test345678");
+
+    const {show} = this.state;
+    this.setState({show : !show})
+  }
+
   render() {
     return (
-      <div>
-        Admin Test
+      <div class='container'>
+          <Header as='h1'> Admin View </Header>
+          <div>
+            <List animated divided size='big' verticalAlign='middle'>
+              <List.Item onClick={this.showUserDetails}>
+                <Image avatar src='../../img/communikey-logo.svg' />
+                <List.Content>
+                  <List.Header as='a'>Daniel Louise</List.Header>
+                </List.Content>
+              </List.Item>
+              {this.state.show && <UserDetails class="UserDetails" />}
+              <List.Item>
+                <Image avatar src='../../img/communikey-logo.svg' />
+                <List.Content>
+                  <List.Header as='a'>Stevie Feliciano</List.Header>
+                </List.Content>
+              </List.Item>
+              <List.Item>
+                <Image avatar src='../../img/communikey-logo.svg' />
+                <List.Content>
+                  <List.Header as='a'>Elliot Fu</List.Header>
+                </List.Content>
+              </List.Item>
+            </List>
+          </div>
+
+
       </div>
     )
   }
