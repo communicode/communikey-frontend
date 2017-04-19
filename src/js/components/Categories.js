4d5b381a19606cf8591e041aacd8d6c6b204dc60
Dennis von der Bey <dvonderbey@communicode.de>
2017-04-19 16:05:00 +0200

    Fixed category view

--- src/js/components/Categories.js
+++ src/js/components/Categories.js
@@ -2,11 +2,14 @@ import React from "react";
 import KeyCard from "./KeyCard";
 import CategoryTree from "./CategoryTree";
 import { Card, Grid, Segment, Divider } from 'semantic-ui-react'
+import "../../css/components/Index.css";
 
 export default class Categories extends React.Component {
   render() {
     return (
-      <CategoryTree/>
+      <div class="container">
+        <CategoryTree/>
+      </div>
     )
   }
 }
