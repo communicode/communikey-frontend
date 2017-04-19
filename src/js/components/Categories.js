3998f984cf4d3a47a4ffcace733d2da88818f643
Dennis von der Bey <dvonderbey@communicode.de>
2017-04-19 15:54:58 +0200

    Fixed treeview & cardview to appear horizontal

--- src/js/components/Categories.js
+++ src/js/components/Categories.js
@@ -1,30 +1,12 @@
 import React from "react";
 import KeyCard from "./KeyCard";
-// import "../../css/components/Index.css";
+import CategoryTree from "./CategoryTree";
 import { Card, Grid, Segment, Divider } from 'semantic-ui-react'
 
 export default class Categories extends React.Component {
   render() {
     return (
-      <div class="content">
-        <Segment raised>
-          <h1>Company internal:</h1>
-          <Divider />
-          <div class="keyGroup">
-            <Grid stackable>
-              <Grid.Column>
-                <KeyCard></KeyCard>
-              </Grid.Column>
-            </Grid>
-          </div>
-          <Divider />
-          <Grid stackable>
-            <Grid.Column>
-              <KeyCard></KeyCard>
-            </Grid.Column>
-          </Grid>
-        </Segment>
-      </div>
+      <CategoryTree/>
     )
   }
 }
