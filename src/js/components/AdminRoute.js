6eefe92c57b8b57ab096a69ac23deed9ffa0ac6b
Dennis von der Bey <dvonderbey@communicode.de>
2017-04-27 15:17:42 +0200

    Implemented Admin Route

---
+++ src/js/components/AdminRoute.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import axios from 'axios'
+import { authStore }  from '../stores/AuthStore';
+import * as constants from '../util/Constants'
+
+class AdminRoute extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  static willTransitionTo(nextState, replace, callback) {
+    var oAuthToken = localStorage.getItem('access_token');
+    axios.get(constants.API_CHECK_PRIVILEGE, {
+       params: {
+          access_token: oAuthToken
+        }
+      })
+      .then(response => {
+        if(response.status == 200 && response.data.privileged == true) {
+          authStore.loggedIn = true;
+          authStore.isAdmin = true;
+          authStore.oAuthToken = oAuthToken;
+          callback();
+        } else if(response.status == 200 && response.data.privileged == false){
+          authStore.setLoggedIn = true;
+          authStore.setIsAdmin = false;
+          replace('/home');
+          callback();
+        }
+        callback();
+      })
+      .catch(error => {
+        console.log(error);
+        authStore.loggedIn = false;
+        authStore.isAdmin = false;
+        replace('/login');
+        callback();
+      });
+  }
+}
+
+export default AdminRoute;
