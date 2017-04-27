ac95b8c24ca4cfa58cd8f70207fe9fc99699f441
Dennis von der Bey <dvonderbey@communicode.de>
2017-04-27 15:18:37 +0200

    Removed example settings page

--- src/js/components/Settings.js
+++ src/js/components/Settings.js
@@ -4,9 +4,7 @@ import AuthenticatedRoute from "./AuthenticatedRoute";
 export default class Settings extends AuthenticatedRoute {
   render() {
     return (
-      <div>
-        Settings Test
-      </div>
+      <div/>
     )
   }
 }
