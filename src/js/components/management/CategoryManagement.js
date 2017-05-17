07453376ac823d50fdd36ea2f754e0883425932a
Sven Greb <sgreb@communicode.de>
2017-05-17 15:55:57 +0200

    CCKEY-121 Update CategoryManagement component

--- src/js/components/management/CategoryManagement.js
+++ src/js/components/management/CategoryManagement.js
@@ -1,19 +1,21 @@
 import React from "react";
-import {inject} from "mobx-react";
-import {Segment, Header, Divider, Button, Accordion, Icon} from "semantic-ui-react";
+import {inject, observer} from "mobx-react";
+import {Segment, Header, Button, Grid, Message, Menu, Container, Rail, Icon} from "semantic-ui-react";
 import "../../../css/components/Admin.css";
 import AdminRoute from "../AdminRoute";
 import CategoryDetailModal from "../modals/CategoryDetailModal";
 import AddCategoryModal from "../modals/AddCategoryModal";
+import CategoryCard from "./../cards/CategoryCard";
+import CategoryTree from "../lists/CategoryTree";
 
-@inject("categoryStore")
+@inject("categoryStore") @observer
 class CategoryManagement extends AdminRoute {
   constructor(props) {
     super(props);
     this.state = {
-      categoryDetailModalIsOpen: false,
+      addCategoryModalIsOpen: false,
       selectedCategory: null,
-      addCategoryModalIsOpen: false
+      categoryDetailModalIsOpen: false
     };
   }
 
@@ -21,12 +23,19 @@ class CategoryManagement extends AdminRoute {
     this.props.categoryStore.fetchCategories();
   }
 
-  toggleCategoryDetailModal = (category) => {
+  toggleCategoryDetailModal = () => {
     const {categoryDetailModalIsOpen} = this.state;
-    this.setState({
-      categoryDetailModalIsOpen: !categoryDetailModalIsOpen,
-      selectedCategory: category
-    })
+    this.setState({categoryDetailModalIsOpen: !categoryDetailModalIsOpen})
+  };
+
+  handleCategoryCardSave = (category) => {
+    this.props.categoryStore.update(category);
+  };
+
+  handleCategoryCardDelete = (category) => {
+    this.props.categoryStore.delete(category.id);
+    this.setState({selectedCategory: null});
+    this.props.categoryStore.fetchCategories();
   };
 
   toggleAddCategoryModal = () => {
@@ -34,44 +43,68 @@ class CategoryManagement extends AdminRoute {
     this.setState({addCategoryModalIsOpen: !addCategoryModalIsOpen})
   };
 
+  toggleVisibility = () => this.setState({visible: !this.state.visible});
+
+  setSelectedCategory = (category) => {
+    this.state.selectedCategory !== category && this.setState({selectedCategory: category});
+  };
+
   render() {
-    //TODO: Remove duplicate
-    // CategoryTree, CategoryManagement & AddChildToCategoryModal use the same function (CategoryList) with different onClick
+    const {categoryStore} = this.props;
+    const {selectedCategory, addCategoryModalIsOpen, categoryDetailModalIsOpen} = this.state;
 
-    let handleClick = (category) => this.toggleCategoryDetailModal(category)
+    const renderCategories = () => (
+      <div>
+        <Container textAlign="left" fluid={true}>
+          <Button icon={<Icon name="add" bordered={false}/>} content="New" labelPosition="left" onClick={this.toggleAddCategoryModal}/>
+        </Container>
+        <Grid centered={true} columns={2}>
+          <Grid.Column>
+            {selectedCategory &&
+            <CategoryCard
+              category={selectedCategory}
+              onSave={this.handleCategoryCardSave}
+              editable={true}
+              onDelete={this.handleCategoryCardDelete}/>
+            }
+            <Rail dividing={true} position="left">
+              <Menu vertical={true} fluid={true}>
+                <CategoryTree categories={categoryStore.categories} onCategorySelect={this.setSelectedCategory}/>
+              </Menu>
+            </Rail>
+          </Grid.Column>
+        </Grid>
+      </div>
+    );
 
-    function CategoryList(props) {
-      if(props.node !== undefined && props.node.length > 0) {
-        let rows = [];
-        props.node.map(function(category) {
-          rows.push(
-            <Accordion fluid activeIndex={0} key={category.id}>
-              <Accordion.Title onClick={() => handleClick(category)}>
-                <Icon name='triangle right' />
-                {category.name}
-              </Accordion.Title>
-              <Accordion.Content>
-                <CategoryList node={category.children} />
-              </Accordion.Content>
-            </Accordion>
-          );
-        })
-        return <tbody>{rows}</tbody>;
-      } else return null
-    }
+    const renderNoCategoriesMessage = () => (
+      <Grid centered={true} columns={1}>
+        <Grid.Column>
+          <Message
+            icon="inbox" compact={true} attached={true}
+            header="There are no categories yet."
+            content="You can create one by clicking on the button below."/>
+          <Button icon="add" attached="bottom" onClick={this.toggleAddCategoryModal}/>
+        </Grid.Column>
+      </Grid>
+    );
+
 
     return (
       <Segment class="adminView">
-        <Header as='h1'>
-          Category management
-          <Button class="headerButton" icon='add' content='Categorie' onClick={this.toggleAddCategoryModal}/>
+        <Header as="h1" textAlign="center">
+          <Header.Content>Categories</Header.Content>
+          <Header.Subheader>Manage category settings and the structure</Header.Subheader>
         </Header>
-        <Divider />
-        <CategoryList node={this.props.categoryStore.categories} />
-        {this.state.addCategoryModalIsOpen && <AddCategoryModal onModalClose={this.toggleAddCategoryModal}/>}
-        {this.state.categoryDetailModalIsOpen && <CategoryDetailModal selectedCategory={this.state.selectedCategory} onModalClose={this.toggleCategoryDetailModal}/>}
+        {categoryStore.categories.length > 0 ? renderCategories() : renderNoCategoriesMessage()}
+        {addCategoryModalIsOpen && <AddCategoryModal onModalClose={this.toggleAddCategoryModal}/>}
+        {categoryDetailModalIsOpen && <CategoryDetailModal
+          selectedCategory={selectedCategory}
+          onModalClose={this.toggleCategoryDetailModal}/>
+        }
       </Segment>
     )
   }
 }
+
 export default CategoryManagement;
