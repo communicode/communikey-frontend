fbfa79b43186e8f51a63409fd72badb23df8c6e7
Sven Greb <sgreb@communicode.de>
2017-05-26 13:48:46 +0200

    CCKEY-140 Add a application-wide config as exported JS module

---
+++ src/js/config/app.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: "communikey",
+  assets: {
+    communicodeLogoTypography: "/assets/images/communicode-logo-typography.svg",
+    communicodeLoaderDots: "/assets/images/communicode-loader-dots.svg",
+    logoCircleDropshadow: "/assets/images/communikey-logo-circle-light-dropshadow.svg",
+    logoLightDropshadow: "/assets/images/communikey-logo-light-dropshadow.svg",
+    logoTypographyGreen: "/assets/images/communikey-logo-typography-green.svg",
+    logoTypographyLight: "/assets/images/communikey-logo-typography-light.svg"
+  }
+};
