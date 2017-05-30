f72e31a9c427d83b675a7bb0b6323af3a1909328
Sven Greb <sgreb@communicode.de>
2017-05-30 20:45:50 +0200

    CCKEY-140 Implement the dashboard route

---
+++ src/routes/Dashboard.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+/**
+ * The dashboard.
+ *
+ * @author sgreb@communicode.de
+ * @since 0.8.0
+ */
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div/>
+    )
+  }
+}
+
+export default Dashboard;
