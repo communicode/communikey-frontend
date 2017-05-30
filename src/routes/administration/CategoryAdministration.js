6957b89a9d0edd78f4c071b077ddbad393f0bfef
Sven Greb <sgreb@communicode.de>
2017-05-30 20:44:28 +0200

    CCKEY-140 Reimplement the administration (management) routes

---
+++ src/routes/administration/CategoryAdministration.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+/**
+ * The administration for categories.
+ *
+ * @author dvonderbey@communicode.de
+ * @author mskyschally@communicode.de
+ * @author sgreb@communicode.de
+ * @since 0.8.0
+ */
+class CategoryAdministration extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div/>
+    )
+  }
+}
+
+export default CategoryAdministration;
---
+++ src/routes/administration/UserAdministration.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+/**
+ * The administration for user.
+ *
+ * @author dvonderbey@communicode.de
+ * @author mskyschally@communicode.de
+ * @author sgreb@communicode.de
+ * @since 0.8.0
+ */
+class UserAdministration extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div/>
+    )
+  }
+}
+
+export default UserAdministration;
