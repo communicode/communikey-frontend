a4ddee040015c8922dcd0fb150bea75cdc46cd75
Sven Greb <sgreb@communicode.de>
2017-05-30 20:25:52 +0200

    CCKEY-140 Implement service constants and request mappings

---
+++ src/services/apiPathVariables.js
@@ -0,0 +1,49 @@
+/**
+ * The path variable for a authority name.
+ *
+ * @constant
+ * @default
+ * @type {string}
+ * @since 0.8.0
+ */
+export const AUTHORITY_NAME = "/${authorityName}";
+
+/**
+ * The path variable for a category ID.
+ *
+ * @constant
+ * @default
+ * @type {string}
+ * @since 0.8.0
+ */
+export const CATEGORY_ID = "/${categoryId}";
+
+/**
+ * The path variable for a key ID.
+ *
+ * @constant
+ * @default
+ * @type {string}
+ * @since 0.8.0
+ */
+export const KEY_ID = "/${keyId}";
+
+/**
+ * The path variable for a user group name.
+ *
+ * @constant
+ * @default
+ * @type {string}
+ * @since 0.8.0
+ */
+export const USER_GROUP_NAME = "/${userGroupName}";
+
+/**
+ * The path variable for a user login.
+ *
+ * @constant
+ * @default
+ * @type {string}
+ * @since 0.8.0
+ */
+export const USER_LOGIN = "/${login}";
