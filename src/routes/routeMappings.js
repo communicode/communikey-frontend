da5e1ac7f152fd5fe2827726f9c505826315e990
Sven Greb <sgreb@communicode.de>
2017-05-30 20:26:55 +0200

    CCKEY-140 Implement route constants and mappings

---
+++ src/routes/routeMappings.js
@@ -0,0 +1,79 @@
+import {
+  ADMINISTRATION_CATEGORIES,
+  ADMINISTRATION_KEYS,
+  ADMINISTRATION_USERS,
+  DASHBOARD,
+  SIGNIN,
+  SIGNOUT,
+  ROOT
+} from "./routeConstants";
+
+/**
+ * The root route mapping.
+ *
+ * @constant
+ * @default
+ * @type {string}
+ * @since 0.8.0
+ */
+export const ROUTE_ROOT = ROOT;
+
+/**
+ * The route mapping for the {@link SignIn} components.
+ *
+ * @constant
+ * @default
+ * @type {string}
+ * @since 0.8.0
+ */
+export const ROUTE_SIGNIN = ROUTE_ROOT + SIGNIN;
+
+/**
+ * The route mapping for the {@link SignOut} components.
+ *
+ * @constant
+ * @default
+ * @type {string}
+ * @since 0.8.0
+ */
+export const ROUTE_SIGNOUT = ROUTE_ROOT + SIGNOUT;
+
+/**
+ * The route mapping for the {@link Dashboard} components.
+ *
+ * @constant
+ * @default
+ * @type {string}
+ * @since 0.8.0
+ */
+export const ROUTE_DASHBOARD = ROUTE_ROOT + DASHBOARD;
+
+/**
+ * The route mapping for the {@link CategoryAdministration} components.
+ *
+ * @constant
+ * @default
+ * @type {string}
+ * @since 0.8.0
+ */
+export const ROUTE_ADMINISTRATION_CATEGORIES = ROUTE_ROOT + ADMINISTRATION_CATEGORIES;
+
+/**
+ * The route mapping for the {@link KeyAdministration} components.
+ *
+ * @constant
+ * @default
+ * @type {string}
+ * @since 0.8.0
+ */
+export const ROUTE_ADMINISTRATION_KEYS = ROUTE_ROOT + ADMINISTRATION_KEYS;
+
+/**
+ * The route mapping for the {@link UserAdministration} components.
+ *
+ * @constant
+ * @default
+ * @type {string}
+ * @since 0.8.0
+ */
+export const ROUTE_ADMINISTRATION_USERS = ROUTE_ROOT + ADMINISTRATION_USERS;
