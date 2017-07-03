9c93fd817917534f6f5eb21fd7db612151af9e28
Dennis von der Bey <dvonderbey@communicode.de>
2017-07-03 14:44:40 +0200

    CCKEY-167 Fixed tests

--- test/components/data/CategoryModal.test.js
+++ test/components/data/CategoryModal.test.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { shallow } from "enzyme";
 import CategoryModal from "../../../src/components/data/CategoryModal";
-import { categories, emptyFunction } from "../../__mockData__/mockUtil";
+import { categories, groups, emptyFunction } from "../../__mockData__/mockUtil";
+import { toJS } from "mobx";
 
 describe('<CategoryModal>', () => {
   test('should render correctly with sample data', () => {
-    const wrapper = shallow(<CategoryModal category={categories[0]} onClose={emptyFunction}/>);
+    const wrapper = shallow(<CategoryModal category={categories[0]} onClose={emptyFunction} userGroups={toJS(groups)}/>);
     expect(wrapper).toMatchSnapshot();
   });
 });
