2dbf0fb5e3309e1bbce64ac647359c62299d8ff7
Dennis von der Bey <dvonderbey@communicode.de>
2017-07-18 13:43:34 +0200

    CCKEY-221 Updated snapshots

--- test/components/data/CategoryModal.test.js
+++ test/components/data/CategoryModal.test.js
@@ -1,12 +1,17 @@
 import React from "react";
-import {shallow} from "enzyme";
+import {mount} from "enzyme";
 import CategoryModal from "../../../src/components/data/CategoryModal";
-import {categoryStore, userGroupStore, emptyFunction} from "../../__mockData__/mockUtil";
+import {categoryStore, userGroupStore, stores, emptyFunction} from "../../__mockData__/mockUtil";
 import {toJS} from "mobx";
+import {Provider} from "mobx-react";
 
 describe("<CategoryModal>", () => {
   test("should render correctly with sample data", () => {
-    const wrapper = shallow(<CategoryModal category={categoryStore.categories[0]} onClose={emptyFunction} userGroups={toJS(userGroupStore.userGroups)}/>);
+    const wrapper = mount(
+      <Provider {...stores}>
+        <CategoryModal category={categoryStore.categories[0]} onClose={emptyFunction} userGroups={toJS(userGroupStore.userGroups)}/>
+      </Provider>
+    );
     expect(wrapper).toMatchSnapshot();
   });
 });
