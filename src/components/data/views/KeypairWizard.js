12102df02758bdb7639ec7c7fbbfca0c2531ffe5
Dennis von der Bey <dvonderbey@communicode.de>
2017-08-14 12:04:16 +0200

    CCKEY-236 Added passphrase input with confirmation

--- src/components/data/views/KeypairWizard.js
+++ src/components/data/views/KeypairWizard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Steps, Button, Row, Card, Icon, Input, Radio} from "antd";
+import {Steps, Button, Row, Col, Card, Icon, Input, Radio, Form} from "antd";
 import FileReaderInput from "react-file-reader-input";
 import {encryptionService} from "../../../Communikey";
 import "antd/lib/radio/style/index.less";
@@ -10,11 +10,57 @@ import "antd/lib/button/style/index.less";
 import "antd/lib/card/style/index.less";
 import "antd/lib/upload/style/index.less";
 import "antd/lib/message/style/index.less";
+import "antd/lib/form/style/index.less";
 import "./KeypairWizard.less";
 
 const Step = Steps.Step;
 const RadioButton = Radio.Button;
 const RadioGroup = Radio.Group;
+
+const ManagedForm = Form.create()(
+  (props) => {
+    const {form, handleSubmit, checkPassword} = props;
+    const {getFieldDecorator} = form;
+    return (
+      <Form hideRequiredMark={true}>
+          <Form.Item
+            validateStatus={form.getFieldError("password") ? "error"  : ""}
+            colon={false}
+          >
+            {getFieldDecorator("password", {
+              rules: [{
+                required: true, message: "Password is required"}]
+            })(
+              <Input
+                name="password"
+                prefix={<Icon type="lock"/>}
+                type="password"
+                placeholder="Password"
+                onPressEnter={handleSubmit}
+              />)
+            }
+          </Form.Item>
+          <Form.Item
+            validateStatus={form.getFieldError("passwordConfirmation") ? "error"  : ""}
+            colon={false}
+          >
+            {getFieldDecorator("passwordConfirmation", {
+              rules: [{required: true, message: "Password confirmation is required"},
+                {validator: checkPassword}]
+            })(
+              <Input
+                name="passwordConfirmation"
+                prefix={<Icon type="lock"/>}
+                type="password"
+                placeholder="Password confirmation"
+                onPressEnter={handleSubmit}
+              />)
+            }
+          </Form.Item>
+      </Form>
+    );
+  }
+);
 
 class KeypairWizard extends React.Component {
   constructor(props) {
@@ -24,10 +70,50 @@ class KeypairWizard extends React.Component {
       useGenerator: true,
       useUpload: true,
       pasteContent: "",
-      selectedMethod: "upload"
+      selectedMethod: "upload",
+      generatorDone: false
     };
   }
 
+  /**
+   * Saves the reference to the managed form component.
+   *
+   * @param form - The form to save the reference to
+   * @since 0.15.0
+   */
+  saveManagedFormRef = (form) => this.form = form;
+
+  /**
+   * Handles the action button click event.
+   *
+   * @since 0.15.0
+   */
+  handleSubmit = () => this.form.validateFields((errors, payload) => {
+    if (!errors) {
+      encryptionService.generateKeypair(payload.password);
+      this.setState({
+        generatorDone: true
+      });
+    }
+  });
+
+  /**
+   * Validator for the form to check the equality of both password input fields
+   *
+   * @since 0.15.0
+   */
+  checkPassword = (rule, value, callback) => {
+    const form = this.form;
+    if (value && value !== form.getFieldValue("password")) {
+      callback("The passwords don't match. Please try again.");
+    } else {
+      callback();
+    }
+  }
+
+  /**
+   * Updates the state item of the private key input box
+   */
   updateInputValue = (event) => {
     this.setState({
       pasteContent: event.target.value
@@ -77,23 +163,32 @@ class KeypairWizard extends React.Component {
     this.next();
   };
 
-  /**
-   * Switches between the upload and paste input boxes.
-   */
-  generateKeypairWithPassphrase = () => {
-    encryptionService.generateKeypair("passphrase");
-  };
-
   render() {
     const generatorContent = () => {
       return (
-        <div>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-          <br/>
-          <br/>
-          <br/>
-          <Button onClick={this.generateKeypairWithPassphrase}>Generate!</Button>
-        </div>
+          <div className="keypairSetup">
+            <Row>
+              Please specify a passphrase and then generate a key by pressing the button.
+              <ManagedForm
+                ref={this.saveManagedFormRef}
+                handleSubmit={this.handleSubmit}
+                checkPassword={this.checkPassword}
+              />
+            </Row>
+            <Row className="actionRow">
+              <Col span={12}>
+                <Button class="buttonPrevious" onClick={this.prev}>
+                  <Icon type="left"/>
+                </Button>
+              </Col>
+              <Col span={12}>
+                <Button class="buttonNext" onClick={this.handleSubmit}>
+                  <Icon type="check"/>
+                  Finish
+                </Button>
+              </Col>
+            </Row>
+          </div>
       );
     };
 
@@ -119,7 +214,7 @@ class KeypairWizard extends React.Component {
      */
     const uploadContent = () => {
       return (
-        <div className="uploadContent">
+        <div className="keypairSetup">
           <Row>
             {
               this.state.useUpload
@@ -146,20 +241,26 @@ class KeypairWizard extends React.Component {
                   </div>
             }
           </Row>
-          <Row>
-            <Button onClick={this.prev}>
-              <Icon type="left"/>
-            </Button>
-            <RadioGroup value={this.state.selectedMethod} size="large">
-              <RadioButton value="upload" onClick={() => this.setState({useUpload: true, selectedMethod: "upload"})}>Upload</RadioButton>
-              <RadioButton value="paste" onClick={() => this.setState({useUpload: false, selectedMethod: "paste"})}>Paste</RadioButton>
-            </RadioGroup>
-            { !this.state.useUpload && this.state.pasteContent &&
-              <Button onClick={this.finishProcedure}>
-                <Icon type="check"/>
-                Finish
+          <Row class="actionRow">
+            <Col span={8}>
+              <Button class="buttonPrevious" onClick={this.prev}>
+                <Icon type="left"/>
               </Button>
-            }
+            </Col>
+            <Col span={8}>
+              <RadioGroup value={this.state.selectedMethod} size="large">
+                <RadioButton value="upload" onClick={() => this.setState({useUpload: true, selectedMethod: "upload"})}>Upload</RadioButton>
+                <RadioButton value="paste" onClick={() => this.setState({useUpload: false, selectedMethod: "paste"})}>Paste</RadioButton>
+              </RadioGroup>
+            </Col>
+            <Col span={8}>
+              { !this.state.useUpload && this.state.pasteContent &&
+                <Button class="buttonNext" onClick={this.finishProcedure}>
+                  <Icon type="check"/>
+                  Finish
+                </Button>
+              }
+            </Col>
           </Row>
         </div>
       );
