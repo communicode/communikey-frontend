f8ff2e3e3c524627e1958f94e287b88a9317851d
Dennis von der Bey <dvonderbey@communicode.de>
2017-09-29 14:23:25 +0200

    CCKEY-248 Added new stores to test, injected

--- test/routes/Dashboard.test.js
+++ test/routes/Dashboard.test.js
@@ -1,11 +1,20 @@
+jest.mock("../../src/Communikey", () => require("../__mockData__/mockUtil"));
 import React from "react";
 import {mount} from "enzyme";
 import Dashboard from "../../src/routes/Dashboard";
+import {Provider} from "mobx-react";
+import {stores} from "../__mockData__/mockUtil";
+import {StaticRouter} from "react-router-dom";
 
 describe("<Dashboard>", () => {
   test("should render correctly", () => {
+    const context = {};
     const wrapper = mount(
-      <Dashboard/>
+      <StaticRouter location="/keys" context={context}>
+        <Provider {...stores}>
+          <Dashboard/>
+        </Provider>
+      </StaticRouter>
     );
     expect(wrapper).toMatchSnapshot();
   });
