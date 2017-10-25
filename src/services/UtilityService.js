4e7247a17f6d2954c494b68c125bc4463b14c5cf
Lauritz Leifermann <lleifermann@communicode.de>
2017-10-25 13:43:15 +0200

    CCKEY-264 Added UtilityService for browser detection

---
+++ src/services/UtilityService.js
@@ -0,0 +1,14 @@
+/**
+ * Browser specific test case that return true if firefox is detected
+ * Firefox implements the InstallTrigger function which is used
+ * for installing extensions.
+ * If it returns undefined, we can be sure the client is not running firefox
+ *
+ *
+ * @constant
+ * @default
+ * @type {boolean}
+ * @since 0.8.0
+ */
+export const IS_FIREFOX = typeof InstallTrigger !== "undefined";
+
